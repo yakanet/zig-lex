f7cf9f32664cf1f123b916f048af1a332c9da35e
Mathieu BROUTIN <yakanet@gmail.com>
2023-08-23 00:03:21 +0200

    feat(lexer): prefer struct instead of union(enum) to represent a Token

--- src/alpha.zig
+++ src/alpha.zig
@@ -1,3 +1,6 @@
+const assert = @import("std").testing;
+
+/// Return true if the value is between '0' and '9'
 pub fn is_digit(value: u8) bool {
     return switch (value) {
         '0'...'9' => true,
@@ -5,6 +8,14 @@ pub fn is_digit(value: u8) bool {
     };
 }
 
+test "is_digit" {
+    try assert.expect(is_digit('0'));
+    try assert.expect(is_digit('1'));
+    try assert.expect(is_digit('9'));
+    try assert.expect(!is_digit('a'));
+}
+
+/// Return true if the value is between 'a' and 'z' (lower, upper) or is a '_'
 pub fn is_alpha(value: u8) bool {
     return switch (value) {
         'a'...'z' => true,
@@ -14,12 +25,21 @@ pub fn is_alpha(value: u8) bool {
     };
 }
 
+test "is_alpha" {
+    try assert.expect(is_alpha('a'));
+    try assert.expect(is_alpha('_'));
+    try assert.expect(is_alpha('z'));
+    try assert.expect(!is_alpha('2'));
+}
+
 pub fn is_alpha_num(value: u8) bool {
     return is_digit(value) or is_alpha(value);
 }
 
+/// List of whitespace characters
 const whitespace = [_]u8{ ' ', '\t', '\n', '\r' };
 
+/// Return true if the value is a whitespace character
 pub fn is_whitespace(value: u8) bool {
     return inline for (whitespace) |w| {
         if (value == w)
@@ -27,22 +47,7 @@ pub fn is_whitespace(value: u8) bool {
     } else false;
 }
 
-const assert = @import("std").testing;
-test "Is digit" {
-    try assert.expect(is_digit('0'));
-    try assert.expect(is_digit('1'));
-    try assert.expect(is_digit('9'));
-    try assert.expect(!is_digit('a'));
-}
-
-test "Is alpha" {
-    try assert.expect(is_alpha('a'));
-    try assert.expect(is_alpha('_'));
-    try assert.expect(is_alpha('z'));
-    try assert.expect(!is_alpha('2'));
-}
-
-test "Is whitespace" {
+test "is_whitespace" {
     try assert.expect(is_whitespace(' '));
     try assert.expect(is_whitespace('\t'));
     try assert.expect(is_whitespace('\r'));
--- src/lexer.zig
+++ src/lexer.zig
@@ -1,15 +1,15 @@
 const std = @import("std");
 const alpha = @import("./alpha.zig");
 
-pub const Token = union(enum) {
+pub const TokenType = enum {
     EOF,
     ILLEGAL,
     LSQUAREBRACE,
     RSQUAREBRACE,
     LPARENTHESIS,
     RPARENTHESIS,
-    IDENTIFIER: []const u8,
-    NUMBER: []const u8,
+    IDENTIFIER,
+    NUMBER,
     COMMA,
     SEMICOLUMN,
     ADD,
@@ -19,17 +19,22 @@ pub const Token = union(enum) {
     ASSIGN,
     LET,
     FUNCTION,
-
-    fn is_keyword(word: []const u8) ?Token {
-        if (std.mem.eql(u8, word, "let")) {
-            return .LET;
-        }
-        if (std.mem.eql(u8, word, "fn")) {
-            return .FUNCTION;
-        }
-        return null;
-    }
 };
+
+pub const Token = struct {
+    token: TokenType,
+    identifier: []const u8 = undefined,
+};
+
+fn is_keyword(word: []const u8) ?TokenType {
+    if (std.mem.eql(u8, word, "let")) {
+        return .LET;
+    }
+    if (std.mem.eql(u8, word, "fn")) {
+        return .FUNCTION;
+    }
+    return null;
+}
 
 pub const Lexer = struct {
     /// Current cursor in input (next item to read)
@@ -87,31 +92,31 @@ pub const Lexer = struct {
         const char = self.char;
         const token: Token = switch (char) {
             0 => return null,
-            '(' => .LPARENTHESIS,
-            ')' => .RPARENTHESIS,
-            '{' => .LSQUAREBRACE,
-            '}' => .RSQUAREBRACE,
-            ',' => .COMMA,
-            ';' => .SEMICOLUMN,
-            '+' => .ADD,
-            '-' => .SUB,
-            '*' => .MUL,
-            '/' => .DIV,
-            '=' => .ASSIGN,
+            '(' => .{ .token = .LPARENTHESIS },
+            ')' => .{ .token = .RPARENTHESIS },
+            '{' => .{ .token = .LSQUAREBRACE },
+            '}' => .{ .token = .RSQUAREBRACE },
+            ',' => .{ .token = .COMMA },
+            ';' => .{ .token = .SEMICOLUMN },
+            '+' => .{ .token = .ADD },
+            '-' => .{ .token = .SUB },
+            '*' => .{ .token = .MUL },
+            '/' => .{ .token = .DIV },
+            '=' => .{ .token = .ASSIGN },
 
             else => blk: {
                 if (alpha.is_digit(char)) {
-                    return .{ .NUMBER = self.parse_number() };
+                    return .{ .token = .NUMBER, .identifier = self.parse_number() };
                 }
                 if (alpha.is_alpha_num(char)) {
                     const identifier = self.parse_identifier();
-                    if (Token.is_keyword(identifier)) |keyword| {
-                        return keyword;
+                    if (is_keyword(identifier)) |keyword| {
+                        return .{ .token = keyword, .identifier = identifier };
                     }
-                    return .{ .IDENTIFIER = identifier };
+                    return .{ .token = .IDENTIFIER, .identifier = identifier };
                 }
 
-                break :blk .ILLEGAL;
+                break :blk .{ .token = .ILLEGAL, .identifier = &[_]u8{} };
             },
         };
         self.read_char();
@@ -123,17 +128,17 @@ test "[Lexer] simple chars" {
     const expectEqualDeep = std.testing.expectEqualDeep;
     var lexer = Lexer.from_input("(){};,+-*/=");
     const tokens = [_]Token{
-        .LPARENTHESIS,
-        .RPARENTHESIS,
-        .LSQUAREBRACE,
-        .RSQUAREBRACE,
-        .SEMICOLUMN,
-        .COMMA,
-        .ADD,
-        .SUB,
-        .MUL,
-        .DIV,
-        .ASSIGN,
+        .{ .token = .LPARENTHESIS },
+        .{ .token = .RPARENTHESIS },
+        .{ .token = .LSQUAREBRACE },
+        .{ .token = .RSQUAREBRACE },
+        .{ .token = .SEMICOLUMN },
+        .{ .token = .COMMA },
+        .{ .token = .ADD },
+        .{ .token = .SUB },
+        .{ .token = .MUL },
+        .{ .token = .DIV },
+        .{ .token = .ASSIGN },
     };
 
     for (tokens) |token| {
@@ -156,30 +161,30 @@ test "[Lexer] simple code" {
     var lexer = Lexer.from_input(code);
 
     var tokens = [_]Token{
-        .LET,
-        .{ .IDENTIFIER = "test" },
-        .ASSIGN,
-        .{ .NUMBER = "5" },
-        .SEMICOLUMN,
+        .{ .token = .LET, .identifier = "let" },
+        .{ .token = .IDENTIFIER, .identifier = "test" },
+        .{ .token = .ASSIGN },
+        .{ .token = .NUMBER, .identifier = "5" },
+        .{ .token = .SEMICOLUMN },
 
-        .LET,
-        .{ .IDENTIFIER = "test2" },
-        .ASSIGN,
-        .{ .NUMBER = "2" },
-        .ADD,
-        .{ .NUMBER = "4" },
-        .SEMICOLUMN,
+        .{ .token = .LET, .identifier = "let" },
+        .{ .token = .IDENTIFIER, .identifier = "test2" },
+        .{ .token = .ASSIGN },
+        .{ .token = .NUMBER, .identifier = "2" },
+        .{ .token = .ADD },
+        .{ .token = .NUMBER, .identifier = "4" },
+        .{ .token = .SEMICOLUMN },
 
-        .FUNCTION,
-        .{ .IDENTIFIER = "demo" },
-        .LPARENTHESIS,
-        .RPARENTHESIS,
-        .LSQUAREBRACE,
-        .{ .NUMBER = "2" },
-        .ADD,
-        .{ .NUMBER = "3" },
-        .SEMICOLUMN,
-        .RSQUAREBRACE,
+        .{ .token = .FUNCTION, .identifier = "fn" },
+        .{ .token = .IDENTIFIER, .identifier = "demo" },
+        .{ .token = .LPARENTHESIS },
+        .{ .token = .RPARENTHESIS },
+        .{ .token = .LSQUAREBRACE },
+        .{ .token = .NUMBER, .identifier = "2" },
+        .{ .token = .ADD },
+        .{ .token = .NUMBER, .identifier = "3" },
+        .{ .token = .SEMICOLUMN },
+        .{ .token = .RSQUAREBRACE },
     };
     for (tokens) |token| {
         try expectEqualDeep(token, lexer.next_token().?);
